f38a1d24278acf024266aeca5e67b552abef068a
Barbara Washington <barbara.m.washington@gmail.com>
2019-01-31 20:55:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
 function showRepositories() {
-  //this is set to the XMLHttpRequest object that fired the event
-  console.log(this.responseText);
-}
- 
-function showRepositories() {
   console.log(this.responseText);
   let repoList = '<ul>';
   for (var i = 0; i < this.responseText.length; i++) {
@@ -11,4 +6,11 @@ function showRepositories() {
   }
   repoList += '</ul>';
   document.getElementById('repositories').innerHTML = repoList;
+}
+ 
+function getRepositories() {
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showRepositories);
+  req.open('GET', 'https://api.github.com/users/octocat/repos');
+  req.send();
 }
